a5395e4e68187f3a4860b6c630d115e4dcf10017
Blith9926 <jpanaranjo9926@hotmail.com>
2026-06-09 20:52:57 -0600

    Update figura.py

--- Semana 4/figura.py
+++ Semana 4/figura.py
@@ -12,17 +12,17 @@ class rectangulo(figura):
     def __init__(self, calcular_area, calcular_perimetro):
         super().__init__(calcular_area, calcular_perimetro)
 #definir el area de un rectangulo
-    def area_rectangulo(self, base, altura):
+    def area(self, base, altura):
         self.base = base
         self.altura = altura
-        area_rectangulo = self.base * self.altura
-        return area_rectangulo
+        area = self.base * self.altura
+        return area
 #definir el perimetro de un rectangulo
-    def perimetro_rectangulo(self, base, altura):
+    def perimetro(self, base, altura):
         self.base = base
         self.altura = altura
-        perimetro_rectangulo = 2 * (self.base + self.altura)
-        return perimetro_rectangulo
+        perimetro = 2 * (self.base + self.altura)
+        return perimetro
 
 
 
@@ -30,15 +30,15 @@ class cuadrado(figura):
     def __init__(self, calcular_area, calcular_perimetro):
         super().__init__(calcular_area, calcular_perimetro)
         #definir el area de un cuadrado
-    def area_cuadrado(self, lado):
+    def area(self, lado):
         self.lado = lado
-        area_cuadrado = self.lado * self.lado
-        return area_cuadrado
+        area = self.lado * self.lado
+        return area
         #definir el perimetro de un cuadrado
-    def perimetro_cuadrado(self, lado):
+    def perimetro(self, lado):
         self.lado = lado
-        perimetro_cuadrado = 4 * self.lado
-        return perimetro_cuadrado
+        perimetro= 4 * self.lado
+        return perimetro
 
 
 
@@ -46,8 +46,8 @@ class cuadrado(figura):
 #crear una instancia de la clase cuadrado
 cuadrado1 = cuadrado("calcular_area", "calcular_perimetro")
 #calcular el area y perimetro de un cuadrado
-area_cuadrado1 = cuadrado1.area_cuadrado(6)
-perimetro_cuadrado1 = cuadrado1.perimetro_cuadrado(6)
+area_cuadrado1 = cuadrado1.area(6)
+perimetro_cuadrado1 = cuadrado1.perimetro(6)
 print(f"El area del cuadrado es: {area_cuadrado1}")
 print(f"El perimetro del cuadrado es: {perimetro_cuadrado1}")
 
@@ -56,7 +56,7 @@ print(f"El perimetro del cuadrado es: {perimetro_cuadrado1}")
 #crear una instancia de la clase rectangulo
 rectangulo1 = rectangulo("calcular_area", "calcular_perimetro") 
 #calcular el area y perimetro de un rectangulo
-area_rectangulo1 = rectangulo1.area_rectangulo(8, 15)
-perimetro_rectangulo1 = rectangulo1.perimetro_rectangulo(8, 15)
+area_rectangulo1 = rectangulo1.area(8, 15)
+perimetro_rectangulo1 = rectangulo1.perimetro(8, 15)
 print(f"El area del rectangulo es: {area_rectangulo1}")
 print(f"El perimetro del rectangulo es: {perimetro_rectangulo1}")
