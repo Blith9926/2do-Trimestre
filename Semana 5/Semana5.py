0943ac167fb8c684ec2e83fe1e865a33bbb3c699
Blith9926 <jpanaranjo9926@hotmail.com>
2026-06-16 20:04:02 -0600

    Update Semana5.py

--- Semana 5/Semana5.py
+++ Semana 5/Semana5.py
@@ -1,1 +1,9 @@
 entrada = "     usuario: Diego | edad: 28 | pais: CR   "
+print(entrada)
+entrada = entrada.strip().capitalize()
+print(entrada)
+nombre = entrada[8:14]
+nombre = nombre.upper()
+print(f"El nombre de usuario es: {nombre}")
+edad = entrada[23:25]
+print(f"La edad del usuario es: {edad}")
